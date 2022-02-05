9f416051918e73572b707d34f387311679cdbd59
Mateo <mateocarriqui7@gmail.com>
2022-02-05 02:09:20 -0300

    Added basic header

--- packages/goldfish/pages/_app.tsx
+++ packages/goldfish/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { AppProps } from 'next/app'
-import Layout from '../components/layout'
+import Layout from '../components/Layout/layout'
 import '../styles/globals.scss'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+
   )
 }
 
