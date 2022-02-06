2da9b59aedd8adcadc0c65defb5138aa23ab39dd
Mateo <mateocarriqui7@gmail.com>
2022-02-06 16:12:09 -0300

    Dropped JWT for httpOnly cookie with user address

--- packages/goldfish/pages/api/auth/wallet.ts
+++ packages/goldfish/pages/api/auth/wallet.ts
@@ -2,7 +2,6 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../lib/prisma'
 import { buildNonceMessage } from '../../../util/web3'
 import { ethers } from 'ethers'
-import jwt from 'jsonwebtoken'
 import Cookies from 'cookies'
 
 const walletApi = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -24,19 +23,7 @@ const walletApi = async (req: NextApiRequest, res: NextApiResponse) => {
     if (user.nonce !== nonce) {
       throw new Error('Wrong Signature')
     } else {
-      const token = jwt.sign(
-        {
-          user_metadata: user,
-          role: 'authenticated',
-        },
-        process.env.JWT_SECRET,
-        {
-          audience: 'authenticated',
-          expiresIn: '1d',
-          subject: user.address,
-        }
-      )
-      new Cookies(req, res).set('session', token, { httpOnly: true })
+      new Cookies(req, res).set('session', user.address, { httpOnly: true })
 
       return res.status(200).json({ user })
     }
